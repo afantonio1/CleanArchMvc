903c5f60cfe6a7efb6f3011e616e5a889d818995
Alessandro <afantonio1@gmail.com>
2023-11-25 16:06:24 -0300

    US - Criação das interfaces de repositórios.

---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetByIdAsync(int? id);
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Product> GetproductCategoryAsync(int? id);
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
